38b23663958b88db7227ce7d00dcb9e8bbcbc2db
james@3bforms.com <james@3bforms.com>
2022-10-10 14:22:47 +0300

    add google tag manager

--- scripts/main.js
+++ scripts/main.js
@@ -23,13 +23,13 @@ export function init() {
             var s1 = document.createElement("script");
             var s0 = document.getElementsByTagName("script")[0];
             s1.async = true;
-            s1.src = 'https://www.googletagmanager.com/gtag/js?id=G-4QK0H4NRS0';
+            s1.src = 'https://www.googletagmanager.com/gtag/js?id=G-15Y9BG0QSZ';
             s1.charset = 'UTF-8';
             s1.setAttribute('crossorigin','*');
             s1.onload = function(){
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-4QK0H4NRS0');
+                gtag('config', 'G-15Y9BG0QSZ');
             }
             s0.parentNode.insertBefore(s1, s0);
     })();
